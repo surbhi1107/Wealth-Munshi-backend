e9a3d7f4e8f461b88c1f744d44064aa070870544
Surbhi Vasoya <vasoyasurbhi@gmail.com>
2024-07-30 22:32:46 +0530

    cors fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-app.use(cors());
+app.use(cors(corsOptions));
 env.config();
 
 app.use(cookieParser());
